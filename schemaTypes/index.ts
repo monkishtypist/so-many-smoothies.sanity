0a7952d8529c0a87f87b737a4bb4d8ee49feee0c
Tim Spinks <tcspinks@gmail.com>
2024-12-09 12:34:06 -0800

    affiliateProduct schema added to index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import smoothie from './smoothie'
+import affiliateProduct from './affiliateProduct';
 
-export const schemaTypes = [post, author, category, blockContent, smoothie]
+export const schemaTypes = [post, author, category, blockContent, smoothie, affiliateProduct]
